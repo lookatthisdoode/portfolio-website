fde32a0703db627cf904891566ea5fcc6099c8b0
lookatthisdoode <radchenko.andreii@gmail.com>
2024-10-23 14:14:13 +0200

    added backup for email service

--- lib/actions.ts
+++ lib/actions.ts
@@ -4,15 +4,20 @@ import {sql} from "@vercel/postgres";
 import sendMail from "./mail-service";
 
 
+// will try to send into DB if not immediately delivered
 export async function createMessage( props:{name:string, contact:string, message:string}) {
     const {name, contact, message} = props
     try {
         await sendMail(props)
-    //     await sql`
-    // INSERT INTO portfolio_messages (name, contact, message)
-    // VALUES (${name}, ${contact}, ${message})
-    // `;
     } catch (error) {
-        throw new Error(`Server action error, ${error}`)
+        try {
+            await sql`
+            INSERT INTO portfolio_messages (name, contact, message)
+            VALUES (${name}, ${contact}, ${message})
+            `;
+        } catch (error) {
+            throw new Error(`Error saving to db ${error}`)
+        }
+        throw new Error(`Error sending email, trying to put into db ${error}`)
     }
 }
